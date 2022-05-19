df1069819e7707bdad66d5fa7749bff3a90eb4c1
Cruspoid <80702064+Cruspoid@users.noreply.github.com>
2022-05-19 15:00:59 -0400

    sus

--- vagonzavod/vagonzavod/settings.py
+++ vagonzavod/vagonzavod/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'vagonzavod.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vagonzavod',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
